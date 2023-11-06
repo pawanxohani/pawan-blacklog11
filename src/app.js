edd3c5c385ce6cea1b830306ce1c6e437f625b03
pawanxohani <146070208+pawanxohani@users.noreply.github.com>
2023-11-06 19:25:47 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("../src/db/connection");
 const router = require("./router/router");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 
 app.use(express.json());
