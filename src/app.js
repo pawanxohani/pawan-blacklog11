8a3d9c77655ba883086761230c92f301599eff1f
pawanxohani <146070208+pawanxohani@users.noreply.github.com>
2023-11-08 17:10:40 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-require('dotenv').config()
 const express = require("express");
 require("../src/db/connection");
 const router = require("./router/router");
+var cors = require('cors')
+
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
-
+app.use(cors({origin : '*'}))
 app.use(express.json());
 app.use("/api/v1/", router);
 
 app.listen(port, () => {
     console.log(`connection is live at port ${port}`);
-  
-});
+})
