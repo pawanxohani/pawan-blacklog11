d742cd95c0d378479c7573bb7fd3e7aa76899d4c
pawanxohani <146070208+pawanxohani@users.noreply.github.com>
2023-11-08 13:07:58 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 require("../src/db/connection");
 const router = require("./router/router");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 
 app.use(express.json());
@@ -11,4 +11,5 @@ app.use("/api/v1/", router);
 
 app.listen(port, () => {
     console.log(`connection is live at port ${port}`);
-})
+    res.end('Mankar is success');
+}).listen(8000);
