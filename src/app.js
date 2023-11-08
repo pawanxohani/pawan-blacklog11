bb2f4a022015ef13043ba0ae02fa4a0db4a13a8c
pawanxohani <146070208+pawanxohani@users.noreply.github.com>
2023-11-08 13:36:16 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,5 +11,5 @@ app.use("/api/v1/", router);
 
 app.listen(port, () => {
     console.log(`connection is live at port ${port}`);
-    res.end('Mankar is success');
+    res.send('Mankar is success');
 });
