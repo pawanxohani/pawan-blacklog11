3f4e6392207c1c46e13ede0058c606ab1f476334
pawanxohani <pawan.d@xohani.com>
2023-11-09 11:50:47 +0530

    .env

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 const express = require("express");
+require('dotenv').config({path : ".env"});
 require("../src/db/connection");
 const router = require("./router/router");
 var cors = require('cors')
 
-const app = express();
+const hostname = process.env.HOSTNAME;
 const port = process.env.PORT || 3000;
+const app = express();
 
 app.use(cors({origin : '*'}))
 app.use(express.json());
 app.use("/api/v1/", router);
 
 app.listen(port, () => {
-    console.log(`connection is live at port ${port}`);
+    console.log(`connection is live at host and port :${hostname}:${port}`);
 })
