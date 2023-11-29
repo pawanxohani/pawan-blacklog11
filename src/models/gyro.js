1bcea7ccfc90386d1e7aab949d81b245fecacd19
pawanxohani <146070208+pawanxohani@users.noreply.github.com>
2023-11-29 15:15:28 +0530

    Update gyro.js
    removed required

--- src/models/gyro.js
+++ src/models/gyro.js
@@ -3,51 +3,39 @@ const mongoose = require('mongoose');
 const gyroSchema = new mongoose.Schema({
     acceloX: {
         type: String,
-        required: true
     },
     acceloY: {
         type: String,
-        required: true
     },
     acceloZ: {
         type: String,
-        required: true
     },
     gyroX: {
         type: String,
-        required: true
     },
     gyroY: {
         type: String,
-        required: true
     },
     gyroZ: {
         type: String,
-        required: true
     },
     temperature: {
         type: String,
-        required: true
     },
     longitude: {
         type: String,
-        required: true
     },
     latitude: {
         type: String,
-        required: true
     },
     speed: {
         type: String,
-        required: true
     },
     time: {
         type: String,
-        required: true
     },
     date: {
         type: String,
-        required: true
     }
 })
 
