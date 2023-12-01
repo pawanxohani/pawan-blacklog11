9b50b26def3d9372cd8eaf522dfda81bf92274d5
pawanxohani <146070208+pawanxohani@users.noreply.github.com>
2023-12-01 12:35:38 +0530

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/gyroDB").then(() => {
     console.log("connection successfully");
 }).catch((e) => {
-    console.log(" No connection successfully");
+    console.log(" No connection ");
 });
