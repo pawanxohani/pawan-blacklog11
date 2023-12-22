1f34c9413c8d6394ac1e12b482b25810ef82de25
pawanxohani <146070208+pawanxohani@users.noreply.github.com>
2023-12-22 13:46:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 const express = require("express");
-require('dotenv').config({path : ".env"});
+const cookieParser = require('cookie-parser');
+require('dotenv').config({ path: ".env" });
 require("../src/db/connection");
 const router = require("./router/router");
-const cors = require('cors')
+const userRouter = require('./router/userRoute');
+const editRouter = require('./router/editRouter');
+
+
+var cors = require('cors')
 
 const hostname = process.env.HOSTNAME;
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors({origin : '*'}))
+app.use(cors({ origin: '*' }))
 app.use(express.json());
 app.use("/api/v1/", router);
+app.use("/api/v1/", userRouter);
+app.use("/api/v1/", editRouter);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
 
 app.listen(port, () => {
     console.log(`connection is live at host and port :${hostname}:${port}`);
